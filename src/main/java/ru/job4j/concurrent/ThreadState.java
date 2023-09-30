6276496cc47f5edc7b8889b4a962c2f90ae6b2c8
Vladislav1992 <kuryanov.vlad@yandex.ru>
2023-09-30 14:56:07 +0300

    3. Состояние нити. [#229175 #448813]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -7,6 +7,11 @@ public class ThreadState {
         first.start();
         second.start();
         System.out.println(Thread.currentThread().getName());
+        while (first.getState() != Thread.State.TERMINATED
+                && second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName() + " - " + first.getState());
+            System.out.println(second.getName() + " - " + second.getState());
+        }
         System.out.println(Thread.currentThread().getName() + " работа завершена");
     }
 }
