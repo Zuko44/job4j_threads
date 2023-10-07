f7eba5fcaad698bf958d8479a11b5f24d5167e6d
Vladislav1992 <kuryanov.vlad@yandex.ru>
2023-10-07 16:46:14 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102 #450712]

--- src/main/java/ru/job4j/io/ReadFile.java
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -1,9 +1,7 @@
 package ru.job4j.io;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public final class ReadFile {
@@ -15,9 +13,9 @@ public final class ReadFile {
 
     public synchronized String getContent(Predicate<Integer> filter) {
         StringBuilder output = new StringBuilder();
-        try (InputStream i = new FileInputStream(file)) {
+        try (BufferedInputStream i = new BufferedInputStream(new FileInputStream(file))) {
             int data;
-            while ((data = i.read()) > 0) {
+            while ((data = i.read()) != -1) {
                 if (filter.test(data)) {
                     output.append((char) data);
                 }
@@ -26,5 +24,14 @@ public final class ReadFile {
             e.printStackTrace();
         }
         return output.toString();
+    }
+
+    public synchronized String firstFilter() throws IOException {
+        Predicate<Integer> filter = data -> data < 0x80;
+        return getContent(filter);
+    }
+
+    public synchronized String withoutFilter() throws IOException {
+        return getContent(Objects::nonNull);
     }
 }
--- src/main/java/ru/job4j/io/WriteFile.java
+++ src/main/java/ru/job4j/io/WriteFile.java
@@ -1,9 +1,6 @@
 package ru.job4j.io;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 
 public final class WriteFile {
     private final File file;
@@ -13,7 +10,7 @@ public final class WriteFile {
     }
 
     public synchronized void saveContent(String content) {
-        try (OutputStream o = new FileOutputStream(file)) {
+        try (BufferedOutputStream o = new BufferedOutputStream(new FileOutputStream(file))) {
             for (int i = 0; i < content.length(); i += 1) {
                 o.write(content.charAt(i));
             }
