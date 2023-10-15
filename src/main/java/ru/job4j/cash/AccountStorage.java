9a77bdcbcfe0fc0ef27416ad70af7c2675df3657
Vladislav1992 <kuryanov.vlad@yandex.ru>
2023-10-15 23:26:48 +0300

    3. Денежные переводы AccountStorage [#1104 #452686]

--- src/main/java/ru/job4j/cash/AccountStorage.java
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -12,13 +12,11 @@ public class AccountStorage {
     private final HashMap<Integer, Account> accounts = new HashMap<>();
 
     public synchronized boolean add(Account account) {
-        accounts.put(account.id(), account);
-        return accounts.containsValue(account);
+        return accounts.putIfAbsent(account.id(), account) != null;
     }
 
     public synchronized boolean update(Account account) {
-        accounts.put(account.id(), account);
-        return accounts.containsValue(account);
+        return accounts.replace(account.id(), accounts.get(account.id()), account);
     }
 
     public synchronized void delete(int id) {
@@ -26,20 +24,16 @@ public class AccountStorage {
     }
 
     public synchronized Optional<Account> getById(int id) {
-        Optional<Account> rsl = Optional.empty();
-        if (accounts.get(id) != null) {
-            rsl = Optional.of(accounts.get(id));
-        }
-        return rsl;
+        return Optional.ofNullable(accounts.get(id));
     }
 
     public synchronized boolean transfer(int fromId, int toId, int amount) {
-        Account from = accounts.get(fromId);
-        Account to = accounts.get(toId);
+        Optional<Account> from = getById(fromId);
+        Optional<Account> to = getById(toId);
         boolean rsl = false;
-        if (from != null && to != null && from.amount() >= amount) {
-            accounts.put(from.id(), new Account(from.id(), from.amount() - amount));
-            accounts.put(to.id(), new Account(to.id(), to.amount() + amount));
+        if (from.isPresent() && to.isPresent() && from.get().amount() >= amount) {
+            accounts.put(from.get().id(), new Account(from.get().id(), from.get().amount() - amount));
+            accounts.put(to.get().id(), new Account(to.get().id(), to.get().amount() + amount));
             rsl = true;
         }
         return rsl;
