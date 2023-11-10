f00a13e98b7eee01e9dc9c9ba0b46e13cd94b941
Vladislav1992 <kuryanov.vlad@yandex.ru>
2023-11-10 16:29:32 +0300

    0. CAS - операции [#6859 #458742]

--- src/main/test/java/ru/job4j/CASCountTest.java
+++ src/main/test/java/ru/job4j/CASCountTest.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CASCountTest {
     @Test
@@ -17,5 +17,43 @@ class CASCountTest {
     void whenIncrementThen0() {
         CASCount cas = new CASCount();
         assertThat(cas.get()).isEqualTo(0);
+    }
+
+    @Test
+    void whenIncrementThen5() {
+        CASCount cas = new CASCount();
+        cas.increment();
+        cas.increment();
+        cas.increment();
+        cas.increment();
+        cas.increment();
+        assertThat(cas.get()).isEqualTo(5);
+    }
+
+    @Test
+    void when3ThreadsThanIncrementThan9() throws InterruptedException {
+        CASCount cas = new CASCount();
+        Thread first = new Thread(() -> {
+            for (int i = 0; i < 3; i++) {
+                cas.increment();
+            }
+        });
+        Thread second = new Thread(() -> {
+            for (int i = 0; i < 3; i++) {
+                cas.increment();
+            }
+        });
+        Thread third = new Thread(() -> {
+            for (int i = 0; i < 3; i++) {
+                cas.increment();
+            }
+        });
+        first.start();
+        second.start();
+        third.start();
+        first.join();
+        second.join();
+        third.join();
+        assertThat(cas.get()).isEqualTo(9);
     }
 }
